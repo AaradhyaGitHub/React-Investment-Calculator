ad81fcd4a16a569d2191827107d46f78f43462a9
AaradhyaGitHub <56614526+AaradhyaGitHub@users.noreply.github.com>
2024-07-27 18:59:12 +0000

    Implemented Use State to store user inputs

--- src/Components/UserInput.jsx
+++ src/Components/UserInput.jsx
@@ -1,53 +1,75 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function UserInput() {
-    const [inputs, setInputs] = useState({
-        'initialInvestment': '',
-        'annualInvestment': '',
-        'expectedReturn': '',
-        'duration': ''
+  const [inputs, setInputs] = useState({
+    initialInvestment: "",
+    annualInvestment: "",
+    expectedReturn: "",
+    duration: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
     });
+  };
 
-    const handleChange = (event) =>{
-        const {name, value} = event.target;
-        setInputs(
-            {
-                ...inputs,
-                [name] : value
-            }
-        );
-    }
-
-
-
-    return (
-        <section id="user-input">
-            <div className="input-group">
-                <p>
-                    <label>Initial Investment</label>
-                    <input type="number" required value={initialInvestment} onChange={handleChange} />
-                </p>
-                <p>
-                    <label>Annual Investment</label>
-                    <input type="number" required value={annualInvestment} onChange={handleChange}/>
-                </p>
-            </div>
-            <div className="input-group">
-                <p>
-                    <label>Expected Return</label>
-                    <input type="number" required value={expectedReturn} onChange={handleChange}/>
-                </p>
-                <p>
-                    <label>Duration</label>
-                    <input type="number" required value={duration} onChange={handleChange}/>
-                </p>
-            </div>
-
-
-        </section>
-
-
-
-
-    )
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            required
+            name="initialInvestment"
+            value={inputs.initialInvestment}
+            onChange={handleChange}
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            required
+            name="annualInvestment"
+            value={inputs.annualInvestment}
+            onChange={handleChange}
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            required
+            name="expectedReturn"
+            value={inputs.expectedReturn}
+            onChange={handleChange}
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            required
+            name="duration"
+            value={inputs.duration}
+            onChange={handleChange}
+          />
+        </p>
+      </div>
+      {/*
+      console.log(
+        inputs.annualInvestment,
+        inputs.initialInvestment,
+        inputs.duration,
+        inputs.expectedReturn
+      )
+        */}
+    </section>
+  );
 }
